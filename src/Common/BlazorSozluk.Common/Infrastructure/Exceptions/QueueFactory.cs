0fda214d6dd8f4cc5aee64010b4d3dd50873d096
sumeyye <sumeyyesabirli@gmail.com>
2023-02-25 16:30:05 +0300

    Favorite ve vote worker

--- src/Common/BlazorSozluk.Common/Infrastructure/Exceptions/QueueFactory.cs
+++ src/Common/BlazorSozluk.Common/Infrastructure/Exceptions/QueueFactory.cs
@@ -41,5 +41,30 @@ namespace BlazorSozluk.Common.Infrastructure.Extensions
             consumer.Model.QueueBind(queueName, exchangeName, queueName);
             return consumer;
         }
+
+        public static EventingBasicConsumer Receive<T>(this EventingBasicConsumer consumer, Action<T> act)
+        {
+            consumer.Received += (m, eventArgs) =>
+            {
+                var body = eventArgs.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+
+                var model = JsonSerializer.Deserialize<T>(message);
+
+                act(model);
+                consumer.Model.BasicAck(eventArgs.DeliveryTag, false);
+            };
+
+            return consumer;
+        }
+
+        public static EventingBasicConsumer StartConsuming(this EventingBasicConsumer consumer, string queueName)
+        {
+            consumer.Model.BasicConsume(queue: queueName,
+                                        autoAck: false,
+                                        consumer: consumer);
+
+            return consumer;
+        }
     }
 }
